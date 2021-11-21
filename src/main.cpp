6b9b656ed311fef1100c40513e78b56db445b0d4
rcaltin <rcaltin@gmail.com>
2021-11-21 12:18:04 +0100

    fix: accidental push removed

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
 #include "app.h"
-#include <restinio/all.hpp>
 
 using namespace restinio;
 
 int main(int argc, char *argv[]) {
-    // restinio::run(
-    //     restinio::on_this_thread()
-    //     .port(8080)
-    //     .address("localhost")
-    //     .request_handler([](auto req) {
-    //         return req->create_response().set_body("Hello, World!").done();
-    //     }));
-
   App app;
   return app.exec(argc, argv);
 }
